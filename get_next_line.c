8a8a605e59b9618486a7f3bf19892c2b679d9d6d
MushVe <c.seguiercuesta@free.fr>
2019-01-10 17:30:59 +0100

    gnl free qui explose

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cseguier <cseguier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 14:45:20 by cseguier          #+#    #+#             */
-/*   Updated: 2019/01/07 16:52:04 by cseguier         ###   ########.fr       */
+/*   Updated: 2019/01/10 17:29:50 by cseguier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,34 @@
 
 static char	*join_free(char *s1, char *s2)
 {
-	char *line;
+	int		i;
+	char	*res;
+	int		t1;
+	int		t2;
 
-	if (!(line = ft_strjoin(s1, s2)))
+	i = 0;
+	if (!s1 || !s2)
 		return (NULL);
-	return (line);
+	t1 = ft_strlen(s1);
+	t2 = ft_strlen(s2);
+	if (!(res = (char*)ft_memalloc(sizeof(char) * (t1 + t2 + 1))))
+		return (0);
+	while (i < t1)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+//	if (t1 > 0)
+//		free(s1);
+	while (i < t1 + t2)
+	{
+		res[i] = s2[i - t1];
+		i++;
+	}
+//	if (t2 > 0)
+//		free(s2);
+	res[i] = '\0';
+	return (res);
 }
 
 static char	*strccpy(char *dest, char const *src, char c)
@@ -39,15 +62,18 @@ static char	*strccpy(char *dest, char const *src, char c)
 static char	*strcdup(char const *src, char c)
 {
 	char	*s;
+	char	*res;
 
 	if (!(s = (char*)ft_memalloc((sizeof(*s) * (ft_strlen(src) + 1)))))
 		return (0);
-	return (strccpy(s, src, c));
+	res = strccpy(s, src, c);
+	return (res);
 }
 
 int			get_next_line(int const fd, char **line)
 {
 	static char	*s;
+	char		*tmp;
 	char		buff[BUFF_SIZE + 1];
 	size_t		lu;
 
@@ -66,7 +92,11 @@ int			get_next_line(int const fd, char **line)
 	if (lu != 0)
 	{
 		*line = strcdup(s, '\n');
-		s = ft_strdup(1 + ft_strchr(s, '\n'));
+		tmp = ft_strdup(1 + ft_strchr(s, '\n'));
+		free(s);
+		*s = *tmp;
+		free(tmp);
+
 	}
 	if (0 < lu)
 		return (1);
